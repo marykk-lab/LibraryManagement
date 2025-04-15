6abe10745303b0745068d3c6a0c56c0a0d0e3a4b
marykk-lab <maksimos0899@gmail.com>
2025-04-15 22:51:12 +0200

    first commit

---
+++ src/test/java/com/library_management/librarymanagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library_management.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
