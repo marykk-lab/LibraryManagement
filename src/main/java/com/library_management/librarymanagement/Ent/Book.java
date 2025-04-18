2f0f2ccbc416cb6878ccec4757169b43e495ba8c
marykk-lab <maksimos0899@gmail.com>
2025-04-18 17:03:07 +0200

    updated entities, added flyway

--- src/main/java/com/library_management/librarymanagement/Ent/Book.java
+++ src/main/java/com/library_management/librarymanagement/Ent/Book.java
@@ -13,7 +13,7 @@ public class Book {
     @Column(name = "book_id")
     private Long bookID;
 
-    @Column(name = "title", length = 50)
+    @Column(name = "book_title", length = 50)
     private String title;
 
     @ManyToOne
@@ -23,4 +23,51 @@ public class Book {
     @OneToMany(mappedBy = "book")
     private Set<Borrower> borrowers;
 
+    public Book(Long bookID, String title, Author author) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(String title, Author author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book() {
+    }
+
+    public Long getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(Long bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookID=" + bookID +
+                ", title='" + title + '\'' +
+                ", author=" + author +
+                ", borrowers=" + borrowers +
+                '}';
+    }
 }
--- src/main/java/com/library_management/librarymanagement/Ent/Borrower.java
+++ src/main/java/com/library_management/librarymanagement/Ent/Borrower.java
@@ -2,15 +2,95 @@ package com.library_management.librarymanagement.Ent;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name="borrow")
 public class Borrower {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long borrower_id;
+    @Column(name = "borrower_id")
+    private Long borrowerID;
+
+
+    private LocalDate borrowingDate;
+    private LocalDate returnDate;
 
     @ManyToOne
     @JoinColumn(name = "book_id")
     private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Borrower(Long borrowerID, LocalDate borrowingDate, LocalDate returnDate, Book book, User user) {
+        this.borrowerID = borrowerID;
+        this.borrowingDate = borrowingDate;
+        this.returnDate = returnDate;
+        this.book = book;
+        this.user = user;
+    }
+
+    public Borrower(LocalDate borrowingDate, LocalDate returnDate, Book book, User user) {
+        this.borrowingDate = borrowingDate;
+        this.returnDate = returnDate;
+        this.book = book;
+        this.user = user;
+    }
+
+    public Borrower() {
+    }
+
+    public Long getBorrowerID() {
+        return borrowerID;
+    }
+
+    public void setBorrowerID(Long borrowerID) {
+        this.borrowerID = borrowerID;
+    }
+
+    public LocalDate getBorrowingDate() {
+        return borrowingDate;
+    }
+
+    public void setBorrowingDate(LocalDate borrowingDate) {
+        this.borrowingDate = borrowingDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Borrower{" +
+                "borrowerID=" + borrowerID +
+                ", borrowingDate=" + borrowingDate +
+                ", returnDate=" + returnDate +
+                ", book=" + book +
+                ", user=" + user +
+                '}';
+    }
 }
